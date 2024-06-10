589889e0b157e9588843ba13440463e822e73957
aiirononeko <aiirononeko@gmail.com>
2024-06-11 00:31:53 +0900

    supabase init

---
+++ app/components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header>
+      <nav>hoge</nav>
+    </header>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
 
+import { Header } from '@/app/components/header'
 import { cn } from '@/lib/utils'
 
 const fontSans = FontSans({
@@ -28,7 +29,8 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        {children}
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   )
