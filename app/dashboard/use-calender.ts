5dec769e915fbf3cd55fe584ea5cd947dd6f34d2
aiirononeko <aiirononeko@gmail.com>
2024-07-24 15:22:58 +0900

    tailwindのclassを動的に当てられない問題を暫定対応

--- app/dashboard/use-calender.ts
+++ app/dashboard/use-calender.ts
@@ -43,7 +43,8 @@ export function useCalendar(
     )
   }
 
-  // MEMO: レンダリングのタイミングによって効いたり効かなかったりする
+  // TODO: TailwindCSSの設定いじって動的に生成できるようにする
+  // MEMO: https://tailwindcss.com/docs/content-configuration#dynamic-class-names
   const getHeight = (
     reservation: Database['public']['Tables']['reservations']['Row'],
   ) => {
@@ -51,15 +52,80 @@ export function useCalendar(
       date(reservation.end_datetime),
       date(reservation.start_datetime),
     )
-    const n = 80 * (diff / 30)
-    return `h-[${n}px]`
+
+    switch (diff / 30) {
+      case 1:
+        return 'h-[80px]'
+      case 2:
+        return 'h-[160px]'
+      case 3:
+        return 'h-[240px]'
+      case 4:
+        return 'h-[320px]'
+      case 5:
+        return 'h-[400px]'
+      case 6:
+        return 'h-[480px]'
+      case 7:
+        return 'h-[560px]'
+      case 8:
+        return 'h-[640px]'
+      case 9:
+        return 'h-[720px]'
+      case 10:
+        return 'h-[800px]'
+      case 11:
+        return 'h-[880px]'
+      case 12:
+        return 'h-[960px]'
+      case 13:
+        return 'h-[1040px]'
+      case 14:
+        return 'h-[1120px]'
+      case 15:
+        return 'h-[1200px]'
+      case 16:
+        return 'h-[1280px]'
+      case 17:
+        return 'h-[1360px]'
+      case 18:
+        return 'h-[1440px]'
+      case 19:
+        return 'h-[1520px]'
+      case 20:
+        return 'h-[1600px]'
+      default:
+        return 'h-[80px]'
+    }
   }
 
-  /*
-   * Storeに設定したキャパシティに応じてグリッドのカラム数を決めます.
-   */
+  // TODO: TailwindCSSの設定いじって動的に生成できるようにする
+  // MEMO: https://tailwindcss.com/docs/content-configuration#dynamic-class-names
   const getGridCols = (maxCapacity: number) => {
-    return `grid-cols-${maxCapacity}`
+    switch (maxCapacity) {
+      case 1:
+        return 'grid-cols-1'
+      case 2:
+        return 'grid-cols-2'
+      case 3:
+        return 'grid-cols-3'
+      case 4:
+        return 'grid-cols-4'
+      case 5:
+        return 'grid-cols-5'
+      case 6:
+        return 'grid-cols-6'
+      case 7:
+        return 'grid-cols-7'
+      case 8:
+        return 'grid-cols-8'
+      case 9:
+        return 'grid-cols-9'
+      case 10:
+        return 'grid-cols-10'
+      default:
+        return 'grid-cols-1'
+    }
   }
 
   return {
