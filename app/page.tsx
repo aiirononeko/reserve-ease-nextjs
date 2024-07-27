159a25a5d80592ce1be366bc2d293b7792b55fa0
aiirononeko <aiirononeko@gmail.com>
2024-07-27 18:59:44 +0900

    LPをPC表示しても崩れないよう修正

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ import Link from 'next/link'
 export const metadata: Metadata = {
   title: 'フリーランス向けの圧倒的低価格な予約システム | ReserveEase',
   description:
-    '月額980円、従量課金なしで利用できるフリーランス・個人店向け予約管理システムです。初月無料、最短3分で導入できるため、今すぐあなたのビジネスの予約を管理できます。',
+    '月額500円、従量課金なしで利用できるフリーランス・個人店向け予約管理システムです。初月無料、最短3分で導入できるため、今すぐあなたのビジネスの予約を管理できます。',
 }
 
 export default function Page() {
   return (
-    <div className='my-8 flex flex-col'>
+    <div className='my-8 flex max-w-96 flex-col md:mx-auto'>
       <div className='mx-4 mb-8 flex flex-col space-y-8'>
         <div className='space-y-4'>
           <div className='relative'>
@@ -33,7 +33,7 @@ export default function Page() {
             <br />
             シンプルで低価格な予約管理を実現。
             <br />
-            完全月額980円でお使いいただけます。
+            完全月額500円でお使いいただけます。
           </p>
         </div>
         <Button variant='default' asChild>
@@ -62,7 +62,7 @@ export default function Page() {
           <h2 className='text-center text-4xl font-bold'>Service</h2>
           <h3 className='text-center text-xl font-bold'>導入メリット</h3>
         </div>
-        <div className='mx-4 grid grid-cols-1 gap-8 text-gray-100 md:grid-cols-2'>
+        <div className='mx-4 grid grid-cols-1 gap-8 text-gray-100'>
           <div className='flex items-center space-x-6'>
             <div className='flex size-14 shrink-0 items-center justify-center rounded-br-2xl bg-gray-100 text-xl font-bold text-primary'>
               01.
@@ -122,21 +122,21 @@ export default function Page() {
           <h2 className='text-center text-4xl font-bold'>Features</h2>
           <h3 className='text-center text-xl font-bold'>機能一覧</h3>
         </div>
-        <div className='mx-4 grid grid-cols-1 gap-8 md:grid-cols-2'></div>
+        <div className='mx-4 grid grid-cols-1 gap-8'></div>
       </div>
       <div className='flex flex-col space-y-8 bg-primary py-8 text-background'>
         <div className='space-y-2'>
           <h2 className='text-center text-4xl font-bold'>Case</h2>
           <h3 className='text-center text-xl font-bold'>導入事例</h3>
         </div>
-        <div className='mx-4 grid grid-cols-1 gap-8 md:grid-cols-2'></div>
+        <div className='mx-4 grid grid-cols-1 gap-8'></div>
       </div>
       <div className='flex flex-col space-y-8 py-8'>
         <div className='space-y-2'>
           <h2 className='text-center text-4xl font-bold'>Flow</h2>
           <h3 className='text-center text-xl font-bold'>導入まで</h3>
         </div>
-        <div className='mx-4 grid grid-cols-1 gap-8 md:grid-cols-2'></div>
+        <div className='mx-4 grid grid-cols-1 gap-8'></div>
       </div>
     </div>
   )
