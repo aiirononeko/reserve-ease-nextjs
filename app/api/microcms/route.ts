1cd2de1d6c9d898ab7373b0e419fa5544349be87
aiirononeko <aiirononeko@gmail.com>
2024-07-27 20:01:04 +0900

    microcmsの更新を検知するwebhook追加

---
+++ app/api/microcms/route.ts
@@ -0,0 +1,58 @@
+import { revalidatePath } from 'next/cache'
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: NextRequest) {
+  const bodyText = await request.text()
+  // const bodyBuffer = Buffer.from(bodyText, "utf-8");
+
+  if (!bodyText) {
+    console.error('Body is empty.')
+    return NextResponse.json({
+      status: 400,
+    })
+  }
+
+  const { id } = JSON.parse(bodyText)
+  console.log(`revalidate ID: ${id}`)
+
+  // const secret = process.env.MICROCMS_WEBHOOK_SIGNATURE_SECRET;
+  // if (!secret) {
+  //   console.error("Secret is empty.");
+  //   return NextResponse.json({
+  //     status: 500,
+  //   });
+  // }
+
+  const signature = request.headers.get('X-MICROCMS-Signature')
+  if (!signature) {
+    console.error('Signature is empty.')
+    return NextResponse.json({
+      status: 400,
+    })
+  }
+
+  // const expectedSignature = crypto
+  //   .createHmac("sha256", secret)
+  //   .update(bodyBuffer)
+  //   .digest("hex");
+  //
+  // const isValid = crypto.timingSafeEqual(
+  //   Buffer.from(signature),
+  //   Buffer.from(expectedSignature),
+  // );
+  //
+  // if (!isValid) {
+  //   console.error("Invalid signature.");
+  //   return NextResponse.json({
+  //     status: 400,
+  //   });
+  // }
+
+  if (id) {
+    revalidatePath(`/articles/${id}`)
+  }
+  revalidatePath('articles')
+
+  return NextResponse.json({ message: 'success' })
+}
--- app/articles/data.ts
+++ app/articles/data.ts
@@ -3,11 +3,8 @@ import { createClient } from '@/lib/microcms'
 export const getArticles = async (): Promise<Article[]> => {
   const microcms = createClient()
 
-  const { contents } = await microcms.get({
+  const { contents } = await microcms.getList({
     endpoint: 'articles',
-    customRequestInit: {
-      cache: 'no-store', // MEMO: MicroCMSの変更をwebhookで受け取るまではキャッシュ使わない
-    },
   })
   return contents
 }
